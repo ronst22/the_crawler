f90d3c0ee87624bdc3304931691bd1f247db8def
eylon shabtay <eylon0060@gmail.com>
2016-03-23 08:11:30 +0200

    facebook crawler: Adding installer and relevant files

---
+++ installer.bat
@@ -0,0 +1,8 @@
+cd Required/
+python-2.7.11.msi
+VCForPython27.msi
+c:\\Python27\\python.exe get-pip.py
+c:\\Python27\\python.exe -m pip install Cython-0.23.4-cp27-none-win32.whl
+c:\\Python27\\python.exe -m pip install numpy-1.10.4+mkl-cp27-cp27m-win32.whl
+c:\\Python27\\python.exe -m pip install scipy-0.17.0-cp27-none-win32
+c:\\Python27\\python.exe setup.py install
