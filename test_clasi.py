49122bbc795aeddce96a764382c0ebd246d6ee25
Ron Stajnrod <ronst12@gmail.com>
2016-03-23 04:07:53 +0200

    Improve efficency of clasifier test

--- test_clasi.py
+++ test_clasi.py
@@ -5,18 +5,17 @@ from Offensive import Offensive
 import pickle
 import unittest
 
-class TestClasifier(unittest.TestCase):
-    def setUp(self):
-        self.offensive = Offensive(clasi=pickle.load(open(os.path.join("test", "movies.pickle"))))
+offensive = Offensive(clasi=pickle.load(open(os.path.join("test", "movies.pickle"))))
 
+class TestClasifier(unittest.TestCase):
     def test_clasi_pos(self):
-        self.assertTrue(self.offensive.is_offensive(open("test/pos/1.txt", 'rt').read()))
+        self.assertTrue(offensive.is_offensive(open("test/pos/1.txt", 'rt').read()))
 
     def test_clasi_neg(self):
-        self.assertFalse(self.offensive.is_offensive(open("test/neg/1.txt", 'rt').read()))
+        self.assertFalse(offensive.is_offensive(open("test/neg/1.txt", 'rt').read()))
 
     def test_clasi_not_really_pos(self):
-        self.assertFalse(self.offensive.is_offensive(open("test/pos/not_real.txt", 'rt').read()))
+        self.assertFalse(offensive.is_offensive(open("test/pos/not_real.txt", 'rt').read()))
 
 if __name__ == '__main__':
     unittest.main()
