617de259a56dd703d3331658c9f81c432c7f875a
Ron Stajnrod <ronst12@Ron-Stajnrods-MacBook-Pro.local>
2016-03-23 02:23:47 +0200

    Add machine learning algorithm framework and test with movies review texts NOTE: This is still not implemented for offensive words, need to get offensive data first

---
+++ Offensive.py
@@ -0,0 +1,18 @@
+import os
+import pickle
+from Classifier.featx import bag_of_words
+
+featx = lambda words: bag_of_words([word.lower() for word in words])
+
+class Offensive():
+    def __init__(self, clasi=None):
+        try:
+            if clasi == None:
+                clasi = pickle.load(open(os.path.join("Classifier", "classifier.pickle")))
+
+            self.clasi = clasi
+        except:
+            print "Failed to read clasifier"
+
+    def is_offensive(self, text):
+        return self.clasi.classify(featx(text.split())) == "pos"
---
+++ test_clasi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+from Offensive import Offensive
+import pickle
+import unittest
+
+class TestClasifier(unittest.TestCase):
+    def setUp(self):
+        self.offensive = Offensive(clasi=pickle.load(open(os.path.join("test", "movies.pickle"))))
+
+    def test_clasi_pos(self):
+        self.assertTrue(self.offensive.is_offensive(open("test/pos/1.txt", 'rt').read()))
+
+    def test_clasi_neg(self):
+        self.assertFalse(self.offensive.is_offensive(open("test/neg/1.txt", 'rt').read()))
+
+    def test_clasi_not_really_pos(self):
+        self.assertFalse(self.offensive.is_offensive(open("test/pos/not_real.txt", 'rt').read()))
+
+if __name__ == '__main__':
+    unittest.main()
